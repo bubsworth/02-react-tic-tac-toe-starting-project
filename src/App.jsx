e696274cc0ae213fc03354c539e861612cd11433
bubsworth <bubbard96@hotmail.com>
2023-11-30 16:12:38 +0000

    Prefer Computed Values & Avoid Unnecessary State Management

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,31 @@
 import { useState } from "react";
 import Player from "./Components/Player";
+import Log from "./Components/Log";
 import GameBoard from "./Components/GameBoard";
 
 function App() {
+  // for logging the games amount of turns taken
+  const [gameTurns, setGameTurns] = useState([]);
   // this initalises the active player
   const [activePlayer, setActivePlayer] = useState("X");
 
   // this will update the active player
-  function handleSelectSquare() {
+  function handleSelectSquare(rowIndex, colIndex) {
     setActivePlayer((curActivePlayer) => (curActivePlayer === "X" ? "O" : "X"));
+    setGameTurns((prevTurns) => {
+      let currentPlayer = "X";
+
+      if (prevTurns.length > 0 && prevTurns[0].player === "X") {
+        currentPlayer = "O";
+      }
+
+      const updatedTurns = [
+        { square: { row: rowIndex, col: colIndex }, player: activePlayer },
+        ...prevTurns,
+      ];
+
+      return updatedTurns;
+    });
   }
 
   return (
@@ -31,7 +48,7 @@ function App() {
           activePlayerSymbol={activePlayer}
         />
       </div>
-      LOG
+      <Log />
     </main>
   );
 }
