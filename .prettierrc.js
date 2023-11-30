75f431423d5e433bd8ac8dcc2637dc158e84bb27
bubsworth <bubbard96@hotmail.com>
2023-11-30 15:16:45 +0000

    Add/update eslint and prettier

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+
+module.exports = {
+  semi: true,
+  trailingComma: "all",
+  singleQuote: true,
+  printWidth: 100,
+  tabWidth: 2,
+  arrowParens: "avoid",
+  endOfLine: "auto",
+  bracketSpacing: true,
+};
